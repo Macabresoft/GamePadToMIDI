58aed0907b59bb10cfaaf0d7794892f4b8f32a34
Brett Story <brettmstory@gmail.com>
2025-02-23 07:23:15 -0600

    Note index shown in corner of MIDI pad.

--- Gameplay/MIDI/MidiPad.cs
+++ Gameplay/MIDI/MidiPad.cs
@@ -15,6 +15,7 @@ public class MidiPad : BaseSpriteEntity, IUpdateableEntity {
     private bool _isOn;
     private MidiNote _midiNote = MidiNote.Empty;
     private MidiSystem? _midiSystem;
+    private ITextRenderer _noteIndexRenderer = EmptyObject.Instance;
 
     /// <summary>
     /// Gets or sets the button for this MIDI pad.
@@ -31,20 +32,40 @@ public class MidiPad : BaseSpriteEntity, IUpdateableEntity {
         }
     }
 
+    /// <inheritdoc />
     public bool ShouldUpdate => this._midiNote.IsEnabled;
 
+    /// <inheritdoc />
     public override byte? SpriteIndex => this._isOn ? this._midiSystem?.PadOnSprite.SpriteIndex : this._midiSystem?.PadOffSprite.SpriteIndex;
 
+    /// <inheritdoc />
     protected override SpriteSheet? SpriteSheet => this._isOn ? this._midiSystem?.PadOnSprite.Asset : this._midiSystem?.PadOffSprite.Asset;
 
+    /// <inheritdoc />
+    public override void Deinitialize() {
+        base.Deinitialize();
+        this._gamePadButtonRenderer = null;
+        this._noteIndexRenderer = EmptyObject.Instance;
+    }
+
+    /// <inheritdoc />
     public override void Initialize(IScene scene, IEntity parent) {
         base.Initialize(scene, parent);
         this._midiSystem = this.Scene.GetSystem<MidiSystem>();
         this.RenderOptions.OffsetType = PixelOffsetType.Center;
+
         this._gamePadButtonRenderer = this.GetOrAddChild<GamePadButtonRenderer>();
+        this._gamePadButtonRenderer.LocalPosition = new Vector2(-0.6f, 0.65f);
+        this._gamePadButtonRenderer.RenderOptions.OffsetType = PixelOffsetType.Center;
+
+        this._noteIndexRenderer = this.GetOrAddChild<TextLineRenderer>();
+        this._noteIndexRenderer.RenderOptions.OffsetType = PixelOffsetType.BottomRight;
+        this._noteIndexRenderer.LocalPosition = new Vector2(0.8f, -0.9f);
+
         this.ResetMidiNote();
     }
 
+    /// <inheritdoc />
     public void Update(FrameTime frameTime, InputState inputState) {
         if (inputState.IsGamePadButtonNewlyPressed(this.Button)) {
             this._midiSystem?.PlayMidiNote(this._midiNote);
@@ -53,13 +74,13 @@ public class MidiPad : BaseSpriteEntity, IUpdateableEntity {
         else if (!inputState.IsGamePadButtonHeld(this.Button)) {
             this._isOn = false;
         }
+
+        this._noteIndexRenderer.Text = this._midiNote.Note.ToString();
     }
 
     private void ResetMidiNote() {
         if (this._gamePadButtonRenderer != null) {
             this._gamePadButtonRenderer.Button = this.Button;
-            this._gamePadButtonRenderer.LocalPosition = new Vector2(-0.6f, 0.65f);
-            this._gamePadButtonRenderer.RenderOptions.OffsetType = PixelOffsetType.Center;
         }
 
         this._midiNote = this._midiSystem?.GetMidiNote(this.Button) ?? MidiNote.Empty;
